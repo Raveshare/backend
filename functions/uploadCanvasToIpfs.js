0bb4dc39f48b507e558769bfda154e547f04a5c9
aryan <agarwalaryan139@gmail.com>
2023-05-02 21:51:18 +0530

    ipfs cors check

--- functions/uploadCanvasToIpfs.js
+++ functions/uploadCanvasToIpfs.js
@@ -1,32 +1,28 @@
-const { NFTStorage , File} = require('nft.storage'); 
+const { NFTStorage, File } = require("nft.storage");
 
 /**
  * A function to upload a canvas to IPFS
- * @param {*} blob Blob of the canvas 
+ * @param {*} blob Blob of the canvas
  * @param {*} name Name of the canvas
- * @param {*} description Description of the canvas
- * @param {*} fileType File type of the canvas 
- * @returns 
+ * @param {*} fileType File type of the canvas
+ * @returns
  */
-async function uploadCanvasToIpfs(blob , name , description,fileType) {
-    const client = new NFTStorage({ token: process.env.NFT_STORAGE_API_KEY });
-    let fileExtension = fileType.split("/")[1];
-
-    name = name.replace(" ", "_");
-    console.log(name);
-
-    try { 
-        const result = await client.store({
-            name: name,
-            description: description,
-            fileType: fileExtension,
-            image : new File([blob], `${name}.${fileExtension}`, { type: fileType })
-        })
-        return result;
-    } catch (error) {
-        console.log(error);
-    }
+async function uploadCanvasToIpfs(blob, name, description, fileType) {
+  const client = new NFTStorage({ token: process.env.NFT_STORAGE_API_KEY });
+  const fileName = name.split(" ").join("");
+  try {
+    const result = await client.store({
+      name: name,
+      description: description,
+      fileType: fileType.split("/")[0],
+      image: new File([blob], `${fileName}.${fileType?.split("/")[1]}`, {
+        type: fileType,
+      }),
+    });
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = uploadCanvasToIpfs;
-
--- index.js
+++ index.js
@@ -1,24 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const db = require('./utils/db/db');
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+
 // include these 2 lines to sync the database
 
 // const sync = require('./utils/db/sync');
 // sync();
 
+app.use(cors());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 
-const contentRouter = require('./routes/content/contentRouter');
-const userRouter = require('./routes/user/userRouter');
-const utilRouter = require('./routes/util/utilRouter');
+const contentRouter = require("./routes/content/contentRouter");
+const userRouter = require("./routes/user/userRouter");
+const utilRouter = require("./routes/util/utilRouter");
 
 app.use(express.json());
 
-app.use('/content', contentRouter);
-app.use('/user', userRouter);
-app.use('/util', utilRouter);
+app.use("/content", contentRouter);
+app.use("/user", userRouter);
+app.use("/util", utilRouter);
 
-app.listen(process.env.PORT || 3000, '0.0.0.0' , () => {
-    console.log("Server started");
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
 });
