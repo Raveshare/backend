066429a3b62edbef0ab21d97de1737a1895c2976
aryan <agarwalaryan139@gmail.com>
2023-05-13 02:03:53 +0530

    remove db sync

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const auth = require('./middleware/auth/auth');
 
 // include these 2 lines to sync the database
 
-const sync = require('./utils/db/sync');
-sync();
+// const sync = require('./utils/db/sync');
+// sync();
 
 app.use(cors());
 app.use(function (req, res, next) {
