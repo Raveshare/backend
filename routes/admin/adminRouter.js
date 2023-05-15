c2e7998195d3a76e71187a676dbe237b820df46a
aryan <agarwalaryan139@gmail.com>
2023-05-16 02:03:42 +0530

    image link add

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -14,6 +14,7 @@ adminRouter.get('/dumpContent', async (req, res) => {
         address: "0x975d74900ef48F53Fa7d4F3550FA0C89f3B3c1Dc",
         name: "wgmis",
         openseaLink: "https://opensea.io/collection/wgmis",
+        image : "https://i.seadn.io/gcs/files/8c0ddbb72f2c23a894218174c8272b72.gif"
     };
 
     let result = await dumpContent("wagmi.json", collectionDetails);
