fa8f2b5d2e94584e0158027eeeb33da29fa90cac
aryan <agarwalaryan139@gmail.com>
2023-06-29 20:39:18 +0530

    fix: image preview

---
+++ functions/updateImagePreview.js
@@ -0,0 +1,39 @@
+const axios = require("axios");
+const canvasSchema = require("../schema/canvasSchema");
+
+const WORKER_URL = process.env.WORKER_URL;
+const WORKER_SECRET = process.env.WORKER_SECRET;
+const updateImagePreview = (canvasData, address, id) => {
+  let data = {
+    data: canvasData,
+    address,
+    id,
+  };
+
+  let url = `${WORKER_URL}/convert`;
+
+  let config = {
+    headers: {
+      Authorization: `${WORKER_SECRET}`,
+    },
+  };
+
+  axios
+    .post(url, data, config)
+    .then((res) => {
+      console.log(res.data);
+      canvasSchema.update(
+        { imageLink: res.data.url },
+        {
+          where: {
+            id: id,
+          },
+        }
+      );
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+module.exports = updateImagePreview;
--- routes/user/canvasRouter.js
+++ routes/user/canvasRouter.js
@@ -1,347 +1,296 @@
-const canvasRouter = require('express').Router();
+const canvasRouter = require("express").Router();
 
-const canvasSchema = require('../../schema/canvasSchema');
-const ownerSchema = require('../../schema/ownerSchema');
+const canvasSchema = require("../../schema/canvasSchema");
+const ownerSchema = require("../../schema/ownerSchema");
 
-const uploadMediaToIpfs = require('../../functions/uploadToIPFS').uploadMediaToIpfs;
-const getImageBuffer = require('../../functions/getImageBuffer')
-const uploadImageToS3 = require('../../functions/uploadImageToS3')
-const uploadToLens = require('../../functions/uploadToLens');
-const uploadToTwitter = require('../../functions/uploadToTwitter');
+const uploadMediaToIpfs =
+  require("../../functions/uploadToIPFS").uploadMediaToIpfs;
+// const getImageBuffer = require('../../functions/getImageBuffer')
+const updateImagePreview = require("../../functions/updateImagePreview");
+const uploadImageToS3 = require("../../functions/uploadImageToS3");
+const uploadToLens = require("../../functions/uploadToLens");
+const uploadToTwitter = require("../../functions/uploadToTwitter");
 
-const canvasCreated = require('../../functions/events/canvasCreated.event');
-const canvasPostedToTwitter = require('../../functions/events/canvasPostedToTwitter.event');
-const canvasPostedToLens = require('../../functions/events/canvasPostedToLens.event');
-const canvasMadePublic = require('../../functions/events/canvasMadePublic.event');
+const canvasCreated = require("../../functions/events/canvasCreated.event");
+const canvasPostedToTwitter = require("../../functions/events/canvasPostedToTwitter.event");
+const canvasPostedToLens = require("../../functions/events/canvasPostedToLens.event");
+const canvasMadePublic = require("../../functions/events/canvasMadePublic.event");
 
-const sendError = require('../../functions/webhook/sendError.webhook');
+const sendError = require("../../functions/webhook/sendError.webhook");
 
-canvasRouter.get('/ping', async (req, res) => {
-    res.send("Canvas Router");
-}
-
-);
-
-canvasRouter.get('/', async (req, res) => {
-    let limit = req.query.limit || 50;
-    let offset = req.query.offset || 0;
-    let address = req.user.address;
-
-    let canvasDatas = await canvasSchema.findAll({
-        limit: limit,
-        offset: offset,
-        order: [
-            ['createdAt']
-        ],
-        where: {
-            ownerAddress: address
-        }
-    });
-
-    res.send(canvasDatas);
+canvasRouter.get("/ping", async (req, res) => {
+  res.send("Canvas Router");
 });
 
-canvasRouter.get('/public', async (req, res) => {
-    let limit = req.query.limit || 50;
-    let offset = req.query.offset || 0;
+canvasRouter.get("/", async (req, res) => {
+  let limit = req.query.limit || 50;
+  let offset = req.query.offset || 0;
+  let address = req.user.address;
 
-    let canvasDatas = await canvasSchema.findAll({
-        limit: limit,
-        offset: offset,
-        order: [
-            ['createdAt']
-        ],
-        where: {
-            isPublic: true
-        }
-    });
+  let canvasDatas = await canvasSchema.findAll({
+    limit: limit,
+    offset: offset,
+    order: [["createdAt"]],
+    where: {
+      ownerAddress: address,
+    },
+  });
 
-    res.send(canvasDatas);
+  res.send(canvasDatas);
 });
 
+canvasRouter.get("/public", async (req, res) => {
+  let limit = req.query.limit || 50;
+  let offset = req.query.offset || 0;
 
+  let canvasDatas = await canvasSchema.findAll({
+    limit: limit,
+    offset: offset,
+    order: [["createdAt"]],
+    where: {
+      isPublic: true,
+    },
+  });
 
-canvasRouter.get('/:id', async (req, res) => {
-    if (!req.params.id) {
-        res.status(400).send({
-            "status": "failed",
-            "message": "Invalid Request Parameters"
-        });
-        return;
-    }
-    let id = req.params.id;
-    let canvasDatas = await canvasSchema.findOne({
-        where: {
-            id: id
-        }
+  res.send(canvasDatas);
+});
+
+canvasRouter.get("/:id", async (req, res) => {
+  if (!req.params.id) {
+    res.status(400).send({
+      status: "failed",
+      message: "Invalid Request Parameters",
+    });
+    return;
+  }
+  let id = req.params.id;
+  let canvasDatas = await canvasSchema.findOne({
+    where: {
+      id: id,
+    },
+  });
+
+  res.send(canvasDatas);
+});
+
+canvasRouter.post("/create", async (req, res) => {
+  let address, canvasData;
+  address = req.user.address;
+  try {
+    canvasData = req.body.canvasData;
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Error: ${error}`);
+    return;
+  }
+  try {
+    canvas = await canvasSchema.create(canvasData);
+
+    let owner = await ownerSchema.findOne({
+      where: {
+        address: address,
+      },
     });
 
-    res.send(canvasDatas);
-})
+    await owner.addCanvas(canvas);
 
-canvasRouter.post('/create', async (req, res) => {
-    let address, canvasData
-    address = req.user.address;
     try {
-        canvasData = req.body.canvasData;
+      let json = JSON.stringify(canvas.data);
+      updateImagePreview(json, address, canvas.id);
     } catch (error) {
-        console.log(error);
-        res.status(500).send(`Error: ${error}`);
-        return;
-    }
-    try {
-        canvas = await canvasSchema.create(canvasData);
-
-        let owner = await ownerSchema.findOne({
-            where: {
-                address: address
-            }
-        });
-
-        await owner.addCanvas(canvas);
-
-        let image;
-        try {
-            let json = JSON.stringify(canvas.data);
-            console.log(json);
-            image = await getImageBuffer(json);
-            if (!image) {
-                res.status(404).send("Canvas image not found");
-                return;
-            }
-        } catch (error) {
-            console.log(error);
-            res.status(500).send(`Error: ${error}`);
-            return;
-        }
-
-        let cid = [], imageLink = [];
-
-        for (let i = 0; i < image.length; i++) {
-            cid.push(await uploadMediaToIpfs(image[i], "image/png"))
-            let filepath = `user/${address}/${canvas.id}-${i}.png`;
-            imageLink.push(await uploadImageToS3(image[i], filepath));
-        }
-
-        console.log(cid, imageLink)
-
-        canvas.ipfsLink = cid;
-        canvas.imageLink = imageLink;
-        await canvas.save();
-
-        res.status(200).send({
-            "status": "success",
-            "message": "Canvas Created",
-            "canvasId": canvas.id
-        });
-
-        canvasCreated(canvas.id, address);
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(`Error: ${error}`);
-        return;
+      console.log(error);
+      res.status(500).send(`Error: ${error}`);
+      return;
     }
 
+    res.status(200).send({
+      status: "success",
+      message: "Canvas Created",
+      canvasId: canvas.id,
+    });
+
+    canvasCreated(canvas.id, address);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Error: ${error}`);
+    return;
+  }
 });
 
-canvasRouter.put('/update', async (req, res) => {
-    let canvasData = req.body.canvasData;
-    let ownerAddress = req.user.address;
+canvasRouter.put("/update", async (req, res) => {
+  let canvasData = req.body.canvasData;
+  let ownerAddress = req.user.address;
 
+  try {
     let canvas = await canvasSchema.findOne({
-        where: {
-            id: canvasData.id
-        }
+      where: {
+        id: canvasData.id,
+      },
     });
 
     if (canvas.ownerAddress != ownerAddress) {
-        res.status(401).send("Unauthorized");
-        return;
+      res.status(401).send("Unauthorized");
+      return;
     }
 
     await canvasSchema.update(canvasData, {
-        where: {
-            id: canvasData.id
-        }
+      where: {
+        id: canvasData.id,
+      },
     });
-
-    let image;
-    try {
-        image = await getImageBuffer(canvas.data);
-        if (!image) {
-            res.status(404).send("Canvas image not found");
-            return;
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(`Error: ${error}`);
-        return;
-    }
-
-    let cid = [], imageLink = [];
-
-    for (let i = 0; i < image.length; i++) {
-        cid.push(await uploadMediaToIpfs(image[i], "image/png"))
-        let filepath = `user/${ownerAddress}/${canvas.id}-${i}.png`;
-        imageLink.push(await uploadImageToS3(image[i], filepath));
-    }
-
-    canvas.ipfsLink = cid;
-    canvas.imageLink = imageLink;
-    await canvas.save();
 
     res.status(200).send("Canvas Updated");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Error: ${error}`);
+    return;
+  }
 });
 
-canvasRouter.put('/visibility', async (req, res) => {
-    let canvasData = req.body.canvasData;
-    let canvasId = canvasData.id;
-    let visibility = canvasData.visibility;
-    let isPublic = false;
+canvasRouter.put("/visibility", async (req, res) => {
+  let canvasData = req.body.canvasData;
+  let canvasId = canvasData.id;
+  let visibility = canvasData.visibility;
+  let isPublic = false;
 
-    if (visibility == "public") {
-        isPublic = true;
+  if (visibility == "public") {
+    isPublic = true;
+  }
+
+  await canvasSchema.update(
+    {
+      isPublic: isPublic,
+    },
+    {
+      where: {
+        id: canvasId,
+      },
     }
+  );
 
-    await canvasSchema.update({
-        isPublic: isPublic
-    }, {
-        where: {
-            id: canvasId
-        }
-    });
+  canvasMadePublic(canvasId, req.user.address);
 
-    canvasMadePublic(canvasId, req.user.address);
-
-    res.status(200).send("Canvas Visibility Updated");
+  res.status(200).send("Canvas Visibility Updated");
 });
 
-canvasRouter.post('/publish', async (req, res) => {
+canvasRouter.post("/publish", async (req, res) => {
+  let canvasData = req.body.canvasData;
+  let ownerAddress = req.user.address;
+  let platform = req.body.platform;
 
-    let canvasData = req.body.canvasData;
-    let ownerAddress = req.user.address;
-    let platform = req.body.platform;
+  let canvasId, name, content;
 
-    let canvasId, name, content;
+  try {
+    canvasId = canvasData.id;
+    name = canvasData.name;
+    content = canvasData.content;
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Error: ${error}`);
+    return;
+  }
 
-    try {
+  if (!platform) {
+    res.status(400).send("Platform not specified");
+    return;
+  }
 
-        canvasId = canvasData.id;
-        name = canvasData.name;
-        content = canvasData.content;
+  let canvas = await canvasSchema.findOne({
+    where: {
+      id: canvasId,
+    },
+  });
 
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(`Error: ${error}`);
-        return;
+  if (canvas.ownerAddress != ownerAddress) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
+
+  let owner = await ownerSchema.findOne({
+    where: {
+      address: ownerAddress,
+    },
+  });
+
+  let json = JSON.stringify(canvas.data);
+
+  if (!json) {
+    res.status(404).send("Canvas data not found");
+    return;
+  }
+
+  let image;
+  try {
+    // image = await getImageBuffer(json);
+    // TODO : integrate with S3
+
+    if (!image) {
+      res.status(404).send("Canvas image not found");
+      return;
     }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(`Error: ${error}`);
+    return;
+  }
 
-    if (!platform) {
-        res.status(400).send("Platform not specified");
-        return;
-    }
+  let cid = [],
+    imageLink = [];
 
+  for (let i = 0; i < image.length; i++) {
+    cid.push(await uploadMediaToIpfs(image[i], "image/png"));
+    let filepath = `user/${ownerAddress}/${canvas.id}-${i}.png`;
+    imageLink.push(await uploadImageToS3(image[i], filepath));
+  }
 
-    let canvas = await canvasSchema.findOne({
-        where: {
-            id: canvasId
-        }
-    });
+  canvas.ipfsLink = cid;
+  canvas.imageLink = imageLink;
+  await canvas.save();
 
-    if (canvas.ownerAddress != ownerAddress) {
-        res.status(401).send("Unauthorized");
-        return;
-    }
+  let resp;
+  if (platform == "lens") {
+    let postMetadata = {
+      name: name,
+      content: content,
+      handle: owner.lens_handle,
+      image: cid,
+    };
 
-    let owner = await ownerSchema.findOne({
-        where: {
-            address: ownerAddress
-        }
-    });
+    resp = await uploadToLens(postMetadata, owner);
+    canvasPostedToLens(canvasId, ownerAddress);
+  } else if (platform == "twitter") {
+    resp = await uploadToTwitter(postMetadata, owner);
+    canvasPostedToTwitter(canvasId, ownerAddress);
+  }
 
-    let json = JSON.stringify(canvas.data);
-
-
-    if (!json) {
-        res.status(404).send("Canvas data not found");
-        return;
-    }
-
-    let image;
-    try {
-        image = await getImageBuffer(json);
-        if (!image) {
-            res.status(404).send("Canvas image not found");
-            return;
-        }
-    } catch (error) {
-        console.log(error);
-        res.status(500).send(`Error: ${error}`);
-        return;
-    }
-
-    let cid = [], imageLink = [];
-
-    for (let i = 0; i < image.length; i++) {
-        cid.push(await uploadMediaToIpfs(image[i], "image/png"))
-        let filepath = `user/${ownerAddress}/${canvas.id}-${i}.png`;
-        imageLink.push(await uploadImageToS3(image[i], filepath));
-    }
-
-    canvas.ipfsLink = cid;
-    canvas.imageLink = imageLink;
-    await canvas.save();
-
-    let resp;
-    if (platform == "lens") {
-
-        let postMetadata = {
-            name: name,
-            content: content,
-            handle: owner.lens_handle,
-            image: cid
-        }
-
-        resp = await uploadToLens(postMetadata, owner);
-        canvasPostedToLens(canvasId, ownerAddress);
-    } else if (platform == "twitter") {
-        resp = await uploadToTwitter(postMetadata, owner);
-        canvasPostedToTwitter(canvasId, ownerAddress);
-    }
-
-
-
-    res.send(resp);
-
+  res.send(resp);
 });
 
-canvasRouter.delete('/delete/:id', async (req, res) => {
-    let canvasId = req.params.id;
-    let ownerAddress = req.user.address;
+canvasRouter.delete("/delete/:id", async (req, res) => {
+  let canvasId = req.params.id;
+  let ownerAddress = req.user.address;
 
-    let canvas = await canvasSchema.findOne({
-        where: {
-            id: canvasId
-        }
-    });
+  let canvas = await canvasSchema.findOne({
+    where: {
+      id: canvasId,
+    },
+  });
 
-    if (canvas.ownerAddress != ownerAddress) {
-        res.status(401).send("Unauthorized");
-        return;
-    }
+  if (canvas.ownerAddress != ownerAddress) {
+    res.status(401).send("Unauthorized");
+    return;
+  }
 
-    await canvasSchema.destroy({
-        where: {
-            id: canvasId
-        }
-    });
+  await canvasSchema.destroy({
+    where: {
+      id: canvasId,
+    },
+  });
 
-    res.status(200).send({
-        "status": "success",
-        "message": "Canvas Deleted"
-    })
+  res.status(200).send({
+    status: "success",
+    message: "Canvas Deleted",
+  });
 });
-
 
 module.exports = canvasRouter;
