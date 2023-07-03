f6ed663b81c9c3fab9212544de04c2dbe5fecbc3
aryan <agarwalaryan139@gmail.com>
2023-07-03 19:35:13 +0530

    add: saving images in s3

--- routes/util/utilRouter.js
+++ routes/util/utilRouter.js
@@ -1,53 +1,23 @@
-const utilRouter = require('express').Router();
-const multer = require('multer');
+const utilRouter = require("express").Router();
+const uploadImageToS3 = require("../../functions/uploadImageToS3");
+const axios = require("axios");
 
-const checkDispatcher = require('../../lens/api').checkDispatcher;
-const ownerSchema = require('../../schema/ownerSchema');
-
-const storage = multer.memoryStorage();
-const upload = multer({
-    storage: storage,
-    limits: {
-        fileSize: 10000000
-    }
+utilRouter.get("/", async (req, res) => {
+  res.send("Util Router");
 });
 
-utilRouter.get('/', async (req, res) => {
-    res.send("Util Router");
-});
+utilRouter.post("/upload-image", async (req, res) => {
+  let { image } = req.query;
 
-utilRouter.post('/updateFollowerCallback', async (req, res) => {
-    let data = req.body;
-    let activity = data.event.activity[0];
-    let toAddress = activity.toAddress;
-    let contractAddress = activity.contractAddress;
-    let event;
-    if (toAddress == "0x0000000000000000000000000000000000000000") {
-        event = "unfollow";
-        tokenId = activity.erc721TokenId;
-    } else {
-        event = "follow";
-        tokenId = activity.erc721TokenId;
-    }
-    let ownerData = await ownerSchema.findOne({
-        where: {
-            followNftAddress: contractAddress
-        }
-    });
-    if (ownerData) {
-        console.log("Owner Data: ", ownerData); ;
-    } else {
-        console.log("Owner Data Not Found");
-    }
-    
+  image = await axios.get(image, { responseType: "arraybuffer" });
 
-    console.log("Follower Callback: ", event, tokenId);
-})
+  image = Buffer.from(image.data, "binary");
 
-utilRouter.get('/checkDispatcher', async (req, res) => {
-    let profileId = req.body.profileId;
-    let result = await checkDispatcher(profileId);
-    res.send(result);
+  let result = await uploadImageToS3(image, `test/${Date.now()}.png`);
+
+  res.send({
+    s3link: result,
+  });
 });
 
 module.exports = utilRouter;
