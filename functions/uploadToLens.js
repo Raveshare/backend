0578e90eccc61622d2261f1dfe56b0ac9126d4cf
aryan <agarwalaryan139@gmail.com>
2023-07-08 13:18:37 +0530

    temp: for logging

--- functions/uploadToLens.js
+++ functions/uploadToLens.js
@@ -1,41 +1,40 @@
-const createPostViaDispatcher = require('../lens/api').createPostViaDispatcher;
-const uploadMetadataToIpfs = require('./uploadToIPFS').uploaddMetadataToIpfs;
-
+const createPostViaDispatcher = require("../lens/api").createPostViaDispatcher;
+const uploadMetadataToIpfs = require("./uploadToIPFS").uploaddMetadataToIpfs;
 
 const uploadToLens = async (postMetadata, ownerData) => {
+  const ipfsData = await uploadMetadataToIpfs(postMetadata);
 
-    const ipfsData = await uploadMetadataToIpfs(postMetadata);
+  let { accessToken, refreshToken } = ownerData.lens_auth_token;
 
-    let { accessToken, refreshToken } = ownerData.lens_auth_token;
+  if (!accessToken || !refreshToken) {
+    res.status(401).send({
+      status: "error",
+      message: "User not authenticated",
+    });
+    return;
+  }
 
-    if (!accessToken || !refreshToken) {
-        res.status(401).send({
-            "status": "error",
-            "message": "User not authenticated"
-        });
-        return;
-    }
+  const createPostRequest = {
+    profileId: ownerData.profileId,
+    contentURI: "ipfs://" + ipfsData,
+    collectModule: {
+      freeCollectModule: { followerOnly: true },
+    },
+    referenceModule: {
+      followerOnlyReferenceModule: false,
+    },
+  };
 
-    const createPostRequest = {
-        "profileId": ownerData.profileId,
-        "contentURI": "ipfs://" + ipfsData,
-        "collectModule": {
-            "freeCollectModule": { "followerOnly": true },
-        },
-        "referenceModule": {
-            "followerOnlyReferenceModule": false,
-        },
-    };
+  const result = await createPostViaDispatcher(
+    createPostRequest,
+    accessToken,
+    refreshToken,
+    ownerData.address
+  );
 
-    const result = await createPostViaDispatcher(
-        createPostRequest,
-        accessToken,
-        refreshToken,
-        ownerData.address
-    );
+  console.log("result", result);
 
-    return result;
-
-}
+  return result;
+};
 
 module.exports = uploadToLens;
