173a5f44d99425389ff495fc9b381e4cddfc9fbc
aryan <agarwalaryan139@gmail.com>
2023-07-08 13:49:13 +0530

    temp: add logging

--- lens/api.js
+++ lens/api.js
@@ -1,10 +1,10 @@
-const { request, gql } = require('graphql-request');
-const ownerSchema = require('../schema/ownerSchema');
+const { request, gql } = require("graphql-request");
+const ownerSchema = require("../schema/ownerSchema");
 
-const LENS_API_URL = 'https://api-mumbai.lens.dev/';
+const LENS_API_URL = "https://api-mumbai.lens.dev/";
 
 const checkDispatcherQuery = gql`
-query Profile($profileId: ProfileId!) {
+  query Profile($profileId: ProfileId!) {
     profile(request: { profileId: $profileId }) {
       dispatcher {
         address
@@ -19,11 +19,10 @@ async function checkDispatcher(profileId) {
   let resp = await request(LENS_API_URL, checkDispatcherQuery, variables);
 
   return resp.profile.dispatcher.canUseRelay;
-
 }
 
 const getFollowContractAddressQuery = gql`
-query Profile($profileId: ProfileId!) {
+  query Profile($profileId: ProfileId!) {
     profile(request: { profileId: $profileId }) {
       followNftAddress
     }
@@ -32,7 +31,11 @@ query Profile($profileId: ProfileId!) {
 
 async function getFollowContractAddress(profileId) {
   const variables = { profileId };
-  let resp = await request(LENS_API_URL, getFollowContractAddressQuery, variables);
+  let resp = await request(
+    LENS_API_URL,
+    getFollowContractAddressQuery,
+    variables
+  );
 
   return resp.profile.followNftAddress;
 }
@@ -43,7 +46,7 @@ const challengeQuery = gql`
       text
     }
   }
-`
+`;
 
 async function challenge(address) {
   const variables = { address };
@@ -51,36 +54,30 @@ async function challenge(address) {
   return resp.challenge.text;
 }
 
-
 const authenticateQuery = gql`
-  mutation Authenticate(
-    $address: EthereumAddress!
-    $signature: Signature!
-  ) {
-    authenticate(request: {
-      address: $address,
-      signature: $signature
-    }) {
+  mutation Authenticate($address: EthereumAddress!, $signature: Signature!) {
+    authenticate(request: { address: $address, signature: $signature }) {
       accessToken
       refreshToken
     }
   }
-`
+`;
 
 const authenticate = async (address, signature) => {
   const variables = { address, signature };
   const result = await request(LENS_API_URL, authenticateQuery, variables);
 
   return result.authenticate;
-}
+};
 
 const getProfileQuery = gql`
-query DefaultProfile($address: EthereumAddress!) {
-  defaultProfile(request: { ethereumAddress: $address}) {
-    handle
-    id
+  query DefaultProfile($address: EthereumAddress!) {
+    defaultProfile(request: { ethereumAddress: $address }) {
+      handle
+      id
+    }
   }
-}`
+`;
 
 async function getProfileHandleAndId(address) {
   const variables = { address };
@@ -88,16 +85,15 @@ async function getProfileHandleAndId(address) {
 
   return {
     handle: resp.defaultProfile.handle,
-    id: resp.defaultProfile.id
-  }
+    id: resp.defaultProfile.id,
+  };
 }
 
 const checkAccessTokenQuery = gql`
-query Query($accessToken : Jwt!) {
-  verify(request: {
-    accessToken: $accessToken
-  })
-}`
+  query Query($accessToken: Jwt!) {
+    verify(request: { accessToken: $accessToken })
+  }
+`;
 
 async function checkAccessToken(accessToken) {
   const variables = { accessToken };
@@ -107,14 +103,13 @@ async function checkAccessToken(accessToken) {
 }
 
 const refreshTokenQuery = gql`
-mutation Refresh($refreshToken: Jwt!) {
-  refresh(request: {
-    refreshToken: $refreshToken
-  }) {
-    accessToken
-    refreshToken
+  mutation Refresh($refreshToken: Jwt!) {
+    refresh(request: { refreshToken: $refreshToken }) {
+      accessToken
+      refreshToken
+    }
   }
-}`
+`;
 
 async function refreshToken(refreshToken) {
   const variables = { refreshToken };
@@ -124,15 +119,13 @@ async function refreshToken(refreshToken) {
 }
 
 const validateMetadataQuery = gql`
-query ValidatePublicationMetadata ($metadatav2: PublicationMetadataV2Input!) {
-  validatePublicationMetadata(request: {
-    metadatav2: $metadatav2
-  }) {
-    valid
-    reason
+  query ValidatePublicationMetadata($metadatav2: PublicationMetadataV2Input!) {
+    validatePublicationMetadata(request: { metadatav2: $metadatav2 }) {
+      valid
+      reason
+    }
   }
-}
-`
+`;
 
 async function validateMetadata(metadatav2) {
   const variables = { metadatav2 };
@@ -142,23 +135,27 @@ async function validateMetadata(metadatav2) {
 }
 
 const createPostViaDispatcherQuery = gql`
-mutation CreatePostViaDispatcher($request: CreatePublicPostRequest!) {
-  createPostViaDispatcher(
-    request: $request
-  ) {
-    ... on RelayerResult {
-      txHash
-      txId
-    }
-    ... on RelayError {
-      reason
+  mutation CreatePostViaDispatcher($request: CreatePublicPostRequest!) {
+    createPostViaDispatcher(request: $request) {
+      ... on RelayerResult {
+        txHash
+        txId
+      }
+      ... on RelayError {
+        reason
+      }
     }
   }
-}`
+`;
 
-async function createPostViaDispatcher(postRequest, accessToken, refreshAccessToken, address) {
+async function createPostViaDispatcher(
+  postRequest,
+  accessToken,
+  refreshAccessToken,
+  address
+) {
   const variables = {
-    "request": postRequest,
+    request: postRequest,
   };
 
   let isAccessTokenValid = await checkAccessToken(accessToken);
@@ -170,23 +167,29 @@ async function createPostViaDispatcher(postRequest, accessToken, refreshAccessTo
     refreshAccessToken = tokens.refreshToken;
 
     const lens_auth_token = {
-      "accessToken": accessToken,
-      "refreshToken": refreshAccessToken
-    }
+      accessToken: accessToken,
+      refreshToken: refreshAccessToken,
+    };
 
     let owner = await ownerSchema.findOne({
       where: {
-        address: address
-      }
-    }
-    );
+        address: address,
+      },
+    });
     owner.lens_auth_token = lens_auth_token;
     await owner.save();
   }
 
-  const result = await request(LENS_API_URL, createPostViaDispatcherQuery, variables, {
-    Authorization: `Bearer ${accessToken}`
-  });
+  const result = await request(
+    LENS_API_URL,
+    createPostViaDispatcherQuery,
+    variables,
+    {
+      Authorization: `Bearer ${accessToken}`,
+    }
+  );
+
+  console.log("result", result);
 
   return result.createPostViaDispatcher;
 }
@@ -200,5 +203,5 @@ module.exports = {
   challenge,
   authenticate,
   getProfileHandleAndId,
-  getFollowContractAddress
-}
+  getFollowContractAddress,
+};
