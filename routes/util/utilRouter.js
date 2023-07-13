ea7ee531e33a03b4272d5b0ac9ff087e59d4c41e
aryan <agarwalaryan139@gmail.com>
2023-07-13 21:53:07 +0530

    add: remove bg endpoint

--- routes/util/utilRouter.js
+++ routes/util/utilRouter.js
@@ -2,27 +2,31 @@ const utilRouter = require("express").Router();
 const uploadImageToS3 = require("../../functions/uploadImageToS3");
 const checkDispatcher = require("../../lens/api").checkDispatcher;
 const axios = require("axios");
+const { removeBackgroundFromImageUrl } = require("remove.bg");
 
 utilRouter.get("/", async (req, res) => {
   res.send("Util Router");
 });
 
-utilRouter.post("/upload-image", async (req, res) => {
+utilRouter.post("/remove-bg", async (req, res) => {
   let { image } = req.query;
 
   if (!image) return res.send({ error: "No image provided" });
 
   try {
-    image = await axios.get(image, { responseType: "arraybuffer" });
+    // image = await axios.get(image, { responseType: "arraybuffer" });
 
-    image = Buffer.from(image.data, "binary");
-  } catch (err) {
-    console.log(err);
-    return res.send({ error: "Error fetching image" });
-  }
+    console.log(image);
 
-  try {
-    let result = await uploadImageToS3(image, `test/${Date.now()}.png`);
+    removebg = await removeBackgroundFromImageUrl({
+      apiKey: process.env.REMOVE_BG_API_KEY,
+      url: image,
+    });
+
+    console.log(removebg);
+
+    let result = await uploadImageToS3(removebg.base64img, `${Date.now()}.png`);
+
     res.send({
       s3link: result,
     });
