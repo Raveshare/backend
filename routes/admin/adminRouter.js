2c21cca38e9200bb1070e0a1073b2e3ee7bb55f4
aryan <agarwalaryan139@gmail.com>
2023-07-16 21:23:05 +0530

    add: to dump all collections

--- routes/admin/adminRouter.js
+++ routes/admin/adminRouter.js
@@ -1,56 +1,96 @@
-const adminRouter = require('express').Router();
-const fs = require('fs');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const adminRouter = require("express").Router();
+const fs = require("fs");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const dumpContent = require('../../scripts/dumpContent');
-const dumpTemplate = require('../../scripts/dumpTemplate');
+const dumpContent = require("../../scripts/dumpContent");
+const dumpTemplate = require("../../scripts/dumpTemplate");
 
-const dumpAsset = require('../../scripts/dumpAsset');
-const deleteAsset = require('../../scripts/deleteAsset');
+const dumpAsset = require("../../scripts/dumpAsset");
+const {
+  deleteNFT,
+  deleteCanvas,
+  deleteTemplate,
+  deleteAsset,
+  deleteCollection,
+} = require("../../scripts/deleteAsset");
 
-adminRouter.get('/', async (req, res) => {
-    res.send("Admin Router");
+adminRouter.get("/", async (req, res) => {
+  res.send("Admin Router");
 });
 
+adminRouter.get("/dumpContent", async (req, res) => {
+  // let { address , name, openseaLink, image , filename } = req.query;
 
-adminRouter.get('/dumpContent', async (req, res) => {
+  let data = [
+    "cryptodickbutt",
+    "cryptotoadz",
+    "goblintown.wtf",
+    "gods-hates-nftees",
+    "mfers",
+    "moonrunners",
+    "nouns",
+    "participants",
+    "rektguy",
+    "synthetic anons",
+    "tubby cats",
+    "wgmis",
+  ];
 
+  // let collectionDetails = {
+  //     address: address,
+  //     name: name,
+  //     openseaLink: openseaLink,
+  //     image : image
+  // };
 
-    let { address , name, openseaLink, image , filename } = req.query;
+  // let filename = `
 
-    let collectionDetails = {
-        address: address,
-        name: name,
-        openseaLink: openseaLink,
-        image : image
-    };
+  try {
+    for (let i = 0; i < data.length; i++) {
+      let filename = data[i] + ".json";
 
-    let result = await dumpContent(filename, collectionDetails);
+      let result = await dumpContent(filename);
 
-    res.send(result);
+      console.log(result);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.send(result);
 });
 
-adminRouter.get('/dumpTemplate' , async (req,res) => {
-    let { name } = req.query;
+adminRouter.get("/dumpTemplate", async (req, res) => {
+  let { name } = req.query;
 
-    let result = await dumpTemplate(name);
+  let result = await dumpTemplate(name);
 
-    res.send(result);
-})
+  res.send(result);
+});
 
-adminRouter.get('/dumpAsset' , async (req,res) => {
-    let { name } = req.query;
+adminRouter.get("/dumpAsset", async (req, res) => {
+  let { name } = req.query;
 
-    let result = await dumpAsset(name);
+  let result = await dumpAsset(name);
 
-    res.send(result);
-})
+  res.send(result);
+});
 
-adminRouter.delete('/deleteAsset' , async (req,res) => {
-    let result = await deleteAsset();
-    res.send(result);
-})
+adminRouter.delete("/deleteAsset", async (req, res) => {
+  // // let result = await deleteNFT();
+  // let result = await deleteTemplate();
+  let result2 = await deleteCanvas();
+  let result = await deleteAsset();
+  res.send(result);
+});
 
+adminRouter.delete("/deleteCollection", async (req, res) => {
+  let id = req.query.id;
+
+  let result = await deleteCollection(id);
+
+  res.send(result);
+});
 
 module.exports = adminRouter;
--- scripts/dumpContent.js
+++ scripts/dumpContent.js
@@ -2,41 +2,40 @@ const db = require("../utils/db/db");
 const { content, collections } = require("../schema/schema");
 const fs = require("fs");
 
-const dumpContent = async (fileName, collectionDetails) => {
+const dumpContent = async (fileName) => {
+  let data = JSON.parse(fs.readFileSync("./dumpdata/collections/" + fileName));
 
-    // await db.sync({ force: true });
+  collectionDetails = data[0];
 
-    let collection = await collections.findOne({
-        where: {
-            address: collectionDetails.address,
-        },
-    });
+  let collection = await collections.findOne({
+    where: {
+      address: collectionDetails.address,
+    },
+  });
 
-    if (!collection) {
-        collection = await collections.create(collectionDetails);
+  if (!collection) {
+    collection = await collections.create(collectionDetails);
+  }
+
+  for (let i = 1; i < data.length; i++) {
+    let contentData = data[i];
+    let contentDetails = {
+      tokenId: contentData.tokenId,
+      title: contentData.title,
+      description: contentData.description,
+      edition: contentData.edition,
+      openseaLink: contentData.openSeaLink,
+      ipfsLink: contentData.ipfsLink,
+      imageURL: contentData.imageURL,
+    };
+    let contentInstance = await content.create(contentDetails);
+    await collection.addContents(contentInstance);
+    if (i % 100 == 0) {
+      console.log("Done with " + i + " records");
     }
+  }
 
-    let data = JSON.parse(fs.readFileSync("./dumpdata/" + fileName));
-    for (let i = 1; i < data.length; i++) {
-        let contentData = data[i];
-        let contentDetails = {
-            tokenId: contentData.tokenId,
-            title: contentData.title,
-            description: contentData.description,
-            edition: contentData.edition,
-            openseaLink: contentData.openSeaLink,
-            ipfsLink: contentData.ipfsLink,
-            imageURL: contentData.imageURL,
-        };
-        let contentInstance = await content.create(contentDetails);
-        await collection.addContents(contentInstance);
-        if (i % 100 == 0) {
-            console.log("Done with " + i + " records");
-        }
-    }
-
-    return "Done";
-
+  return "Done";
 };
 
 // example usage
