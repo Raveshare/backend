e0ddfb6baea02e3175c40a11be7d080f5c3aa416
Aryan Agarwal <aryan@192.168.1.12>
2023-07-24 21:55:50 +0530

    fix: dispatcher to use jwt user

--- routes/util/utilRouter.js
+++ routes/util/utilRouter.js
@@ -1,7 +1,7 @@
 const utilRouter = require("express").Router();
 const uploadImageToS3 = require("../../functions/uploadImageToS3");
 const checkDispatcher = require("../../lens/api").checkDispatcher;
-const axios = require("axios");
+const ownerSchema = require("../../schema/ownerSchema");
 const { removeBackgroundFromImageUrl } = require("remove.bg");
 const getIsWhitelisted = require("../../functions/getIsWhitelisted");
 const auth = require("../../middleware/auth/auth");
@@ -10,20 +10,18 @@ utilRouter.get("/", async (req, res) => {
   res.send("Util Router");
 });
 
-utilRouter.post("/remove-bg",auth,  async (req, res) => {
+utilRouter.post("/remove-bg", auth, async (req, res) => {
   let { image } = req.query;
 
   if (!image) return res.send({ error: "No image provided" });
 
   try {
-
     removebg = await removeBackgroundFromImageUrl({
       apiKey: process.env.REMOVE_BG_API_KEY,
       url: image,
     });
 
     let imageBuffer = Buffer.from(removebg.base64img, "base64");
-
 
     let result = await uploadImageToS3(imageBuffer, `temp/${Date.now()}.png`);
 
@@ -38,24 +36,32 @@ utilRouter.post("/remove-bg",auth,  async (req, res) => {
 });
 
 utilRouter.get("/check-dispatcher", auth, async (req, res) => {
-  const { profileId } = req.query;
+  let ownerAddress = req.user.address;
 
-  console.log("profileId", profileId);
+  let owner = await ownerSchema.findOne({ address: ownerAddress });
+
+  let profileId = owner.profileId;
+
+  if (!profileId)
+    return res.send({
+      status: "error",
+      message: "No profileId found",
+    });
 
   const result = await checkDispatcher(profileId);
 
   res.send(result);
 });
 
-utilRouter.get("/whitelisted" , async(req,res) => {
+utilRouter.get("/whitelisted", async (req, res) => {
   const { wallet } = req.query;
 
   let isWhitelisted = await getIsWhitelisted(wallet);
 
   res.send({
-    "status" : "success",
-    "message" : isWhitelisted
+    status: "success",
+    message: isWhitelisted,
   });
-})
+});
 
 module.exports = utilRouter;
