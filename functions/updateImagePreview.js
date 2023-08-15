46c1d966ce2f354b2e81dbbc60558c4679728845
Aryan Agarwal <aryan@Aryans-MacBook-Air.local>
2023-08-15 21:43:23 +0530

    fix: update image preview

--- functions/updateImagePreview.js
+++ functions/updateImagePreview.js
@@ -3,22 +3,26 @@ const uploadImageToS3 = require("./uploadImageToS3");
 const { uploadMediaToIpfs } = require("./uploadToIPFS");
 
 const updateImagePreview = async (previewData, address, id) => {
+  try {
   let filename = `user/${address}/canvases/${id}.png`;
   previewData = Buffer.from(previewData, "base64");
   console.log(previewData);
   let url = await uploadImageToS3(previewData, filename);
   let ipfs = await uploadMediaToIpfs(previewData);
 
-  console.log(url , ipfs);
+  console.log(url, ipfs);
 
   canvasSchema.update(
-    { imageLink: url , ipfsLink: ipfs },
+    { imageLink: [url], ipfsLink: [ipfs] },
     {
       where: {
         id: id,
       },
     }
   );
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 module.exports = updateImagePreview;
