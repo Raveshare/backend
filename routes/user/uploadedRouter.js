b814b787addf2864910fdd6dc00553d19a410c32
Aryan Agarwal <aryan@Aryans-MacBook-Air.local>
2023-08-17 20:59:48 +0530

    add: upload router

---
+++ routes/user/uploadedRouter.js
@@ -0,0 +1,100 @@
+const uploadedRouter = require("express").Router();
+const uploadedSchema = require("../../schema/uploaded.schema");
+const uploadImageToS3 = require("../../functions/uploadImageToS3");
+const userRouter = require("./userRouter");
+
+uploadedRouter.post("/", async (req, res) => {
+  let address = req.user.address;
+  let { image } = req.body;
+
+  if (!image) return res.status(404).send({ error: "No image provided" });
+
+  try {
+    let imageBuffer = Buffer.from(image, "base64");
+
+    let result = await uploadImageToS3(
+      imageBuffer,
+      `user/${address}/user_assets/${Date.now()}.png`
+    );
+
+    await uploadedSchema.create({
+      address: address,
+      image: result,
+    });
+
+    res.send({
+      s3link: result,
+    });
+    return;
+  } catch (err) {
+    console.log(err);
+    return res.status(503).send({ error: "Error uploading image" });
+  }
+});
+
+uploadedRouter.get("/", async (req, res) => {
+  let address = req.user.address;
+
+  let page = req.query.page || 1;
+
+  let limit = 50;
+  let offset = (page - 1) * limit;
+
+  let uploaded = await uploadedSchema.findAll({
+    where: {
+      address: address,
+    },
+    limit: limit,
+    offset: offset,
+  });
+
+  let count = await uploadedSchema.count({
+    where: {
+        address: address,
+    }
+    });
+
+    let pages = Math.ceil(count / limit);
+
+  res.send({
+    message : uploaded,
+    page: page,
+    nextPage : page < pages ? page + 1 : null,
+  });
+});
+
+uploadedRouter.delete("/:id", async (req, res) => {
+  let address = req.user.address;
+  let id = req.params.id;
+
+  if (!id) {
+    res.status(400).send({
+      status: "error",
+      message: "Invalid Request Parameters",
+    });
+    return;
+  }
+
+  let uploadedData = await uploadedSchema.findOne({
+    where: {
+      id: id,
+      address: address,
+    },
+  });
+
+  if (!uploadedData) {
+    res.status(400).send({
+      status: "error",
+      message: "Invalid Request Parameters",
+    });
+    return;
+  }
+
+  await uploadedData.destroy();
+
+  res.status(200).send({
+    message : "Deleted Successfully",
+  });
+});
+
+module.exports = uploadedRouter;
--- routes/user/userRouter.js
+++ routes/user/userRouter.js
@@ -1,9 +1,11 @@
 const userRouter = require('express').Router();
 const nftRouter = require('./nftRouter');
 const canvasRouter = require('./canvasRouter');
+const uploadedRouter = require('./uploadedRouter');
 const auth = require('../../middleware/auth/auth');
 
 userRouter.use('/nft', auth, nftRouter);
 userRouter.use('/canvas',auth, canvasRouter);
+userRouter.use('/upload',auth, uploadedRouter);
 
 module.exports = userRouter;
