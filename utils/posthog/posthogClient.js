3204224e52679c9fc4d587c06944a925850a77a3
Aryan Agarwal <aryan@Aryans-MacBook-Air.local>
2023-08-18 15:10:20 +0530

    fix: posthog api

--- utils/posthog/posthogClient.js
+++ utils/posthog/posthogClient.js
@@ -2,7 +2,7 @@ const { PostHog } = require('posthog-node')
 const POSTHOG_API_KEY = process.env.POSTHOG_API_KEY
 
 const posthogClient = new PostHog(POSTHOG_API_KEY, {
-    host: 'https://app.posthog.com',
+    host: 'https://eu.posthog.com',
 })
 
 module.exports = posthogClient
