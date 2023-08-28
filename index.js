27c7c88dfd33cde5672c4687a1fcd2e6a8b6b01b
Aryan Agarwal <aryan@Aryans-MacBook-Air.local>
2023-08-28 10:44:21 +0530

    fix: admin route on dev

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const bree = require("./utils/scheduler/scheduler");
+const dotenv = require("dotenv");
+dotenv.config();
 const app = express({
   logger: true,
 });
@@ -7,7 +9,6 @@ const app = express({
 (async () => {
   await bree.start();
 })();
-
 
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
@@ -18,6 +19,10 @@ const auth = require("./middleware/auth/auth");
 // include these 2 lines to sync the database
 // const sync = require("./utils/db/sync")
 // sync()
+
+
+const NODE_ENV = process.env.NODE_ENV
+console.log("NODE_ENV", NODE_ENV);
 
 app.use(cors());
 app.use(function (req, res, next) {
@@ -34,7 +39,6 @@ app.use(function (req, res, next) {
 const collectionRouter = require("./routes/content/collectionRouter");
 const userRouter = require("./routes/user/userRouter");
 const utilRouter = require("./routes/util/utilRouter");
-const adminRouter = require("./routes/admin/adminRouter");
 const authRouter = require("./routes/auth/authRouter");
 const templateRouter = require("./routes/content/templateRouter");
 const assetRouter = require("./routes/content/assetRouter");
@@ -42,13 +46,18 @@ const assetRouter = require("./routes/content/assetRouter");
 app.use(express.json({ limit: "20mb", extended: true }));
 app.use(handleError);
 
+
 app.use("/collection", auth, collectionRouter);
 app.use("/user", userRouter);
-app.use("/admin", adminRouter);
 app.use("/auth", authRouter);
 app.use("/util", utilRouter);
 app.use("/template", auth, templateRouter);
 app.use("/asset", auth, assetRouter);
+
+if(NODE_ENV === "local") {
+  const adminRouter = require("./routes/admin/adminRouter");
+  app.use("/admin", adminRouter);
+}
 
 app.listen(process.env.PORT || 3001, "0.0.0.0", () => {
   console.log("Server started on port 3000");
