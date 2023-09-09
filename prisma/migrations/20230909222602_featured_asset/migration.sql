fc1c601fac964823cbd22baf8535bcce5234843d
aryan <agarwalaryan139@gmail.com>
2023-09-09 22:36:12 +0000

    feat: asset featured + wallet

---
+++ prisma/migrations/20230909222602_featured_asset/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Assets" ADD COLUMN     "featured" BOOLEAN DEFAULT false,
+ADD COLUMN     "wallet" VARCHAR(255);
--- src/schema/assetSchema.js
+++ src/schema/assetSchema.js
@@ -14,12 +14,20 @@ const Asset = db.define('Asset', {
         type : DataTypes.STRING,
         allowNull : true
     },
+    wallet : {
+        type : DataTypes.STRING,
+        allowNull : true
+    },
     image : {
         type : DataTypes.STRING,
         allowNull : true
     },
     dimensions : {
         type : DataTypes.ARRAY(DataTypes.INTEGER),
+        allowNull : true
+    },
+    featured : {
+        type : DataTypes.BOOLEAN,
         allowNull : true
     },
 });
