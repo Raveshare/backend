d3385e8bbce8bad75a5fe22e1f7683fda920609b
aryan <agarwalaryan139@gmail.com>
2023-10-14 17:41:18 +0530

    fix: typo , ugh

--- src/routes/auth/authRouter.js
+++ src/routes/auth/authRouter.js
@@ -2,7 +2,7 @@ const authRouter = require("express").Router();
 const evmRouter = require("./evmRouter");
 const solanaRouter = require("./solanaRouter");
 
-authRouter.use("/emv", evmRouter);
+authRouter.use("/evm", evmRouter);
 authRouter.use("/solana", solanaRouter);
 
 module.exports = authRouter;
