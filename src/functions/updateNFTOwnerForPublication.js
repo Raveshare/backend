91fd332ccf337451f27182be4d5db5cb9e72d7f0
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2023-10-25 01:09:19 +0530

    feat: Implemented Caching

--- src/functions/updateNFTOwnerForPublication.js
+++ src/functions/updateNFTOwnerForPublication.js
@@ -1,5 +1,6 @@
 const { Alchemy, Network } = require("alchemy-sdk");
 const canvasSchema = require("../schema/canvasSchema");
+const { getCache, setCache } = require("../functions/handleCache");
 
 const config = {
   apiKey: process.env.ALCHEMY_API_KEY,
@@ -9,24 +10,31 @@ const alchemy = new Alchemy(config);
 
 const updateNFTOwnerForPublication = async (contractAddress, canvasId) => {
   const { owners } = await alchemy.nft.getOwnersForContract(contractAddress);
+  let canvasownerCache = getCache(`canvas_${canvasId}`);
+  let canvas;
 
-  let canvas = await canvasSchema.findOne({
-    where: {
-      id: canvasId,
-    },
-  });
+  if (canvasownerCache) {
+    canvas = JSON.parse(canvasownerCache);
+  } else {
+    canvas = await canvasSchema.findOne({
+      where: {
+        id: canvasId,
+      },
+    });
+
+    await setCache(`canvas_${canvasId}`, JSON.stringify(canvas));
+  }
 
   let allowList = canvas.allowList;
   allowList = allowList.concat(owners);
   canvas.allowList = allowList;
 
   let gatedWith = canvas.gatedWith;
-  if(!gatedWith.includes(contractAddress)) 
-  gatedWith = gatedWith.concat(contractAddress);
+  if (!gatedWith.includes(contractAddress))
+    gatedWith = gatedWith.concat(contractAddress);
   canvas.gatedWith = gatedWith;
 
   await canvas.save();
-  
 };
 
 module.exports = updateNFTOwnerForPublication;
