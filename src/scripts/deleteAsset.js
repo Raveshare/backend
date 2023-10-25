0bddf2cfcfced4687c143fb1a09b0aca8596033d
aryan <agarwalaryan139@gmail.com>
2023-10-25 13:32:07 +0400

    feat: add update canvas script

--- src/scripts/deleteAsset.js
+++ src/scripts/deleteAsset.js
@@ -1,62 +1,69 @@
-// const fs = require("fs");
-// const assetSchema = require("../schema/assetSchema");
-// const nftSchema = require("../schema/nftSchema");
-// const canvasSchema = require("../schema/canvasSchema");
-// const templateSchema = require("../schema/templateSchema");
+const prisma = require("../prisma");
 
-// const content = require("../schema/content");
-// const collections = require("../schema/collections");
+async function deleteAsset() {
+  assetSchema.destroy({
+    where: {
+      author: "nouns",
+    },
+  });
+}
 
-// async function deleteAsset() {
-//   assetSchema.destroy({
-//     where: {
-//       author: "nouns",
-//     },
-//   });
-// }
+async function updateCanvas() {
+  const owners = await prisma.owners.findMany();
 
-// async function deleteNFT() {
-//   nftSchema.destroy({
-//     where: {
-//       permaLink: "https://shibnft.cc/nft2.png"
-//     }
-//   });
-// }
+  for (const owner of owners) {
+    await prisma.canvases.updateMany({
+      where: {
+        ownerAddress: owner.address,
+      },
+      data: {
+        ownerId: owner.id,
+      },
+    });
+  }
+}
 
-// async function deleteCanvas() {
-//   canvasSchema.destroy({
-//     where: {
-//       imageLink: null,
-//     },
-//     truncate: true,
-//   });
-// }
+async function deleteNFT() {
+  await prisma.nftData.deleteMany({
+    where: {
+      ownerId: null,
+    },
+  });
+}
 
-// async function deleteTemplate() {
-//   templateSchema.destroy({
-//     where: {},
-//     truncate: true,
-//   });
-// }
+async function deleteCanvas() {
+  canvasSchema.destroy({
+    where: {
+      imageLink: null,
+    },
+    truncate: true,
+  });
+}
 
-// async function deleteCollection(id) {
-//   await content.destroy({
-//     where: {
+async function deleteTemplate() {
+  templateSchema.destroy({
+    where: {},
+    truncate: true,
+  });
+}
 
-//     }
-//   });
+async function deleteCollection(id) {
+  await content.destroy({
+    where: {},
+  });
 
-//   await collections.destroy({
-//     where: {
-//       // id : id
-//     },
-//   });
-// }
+  await collections.destroy({
+    where: {
+      // id : id
+    },
+  });
+}
 
-// module.exports = {
-//   deleteAsset,
-//   deleteNFT,
-//   deleteCanvas,
-//   deleteTemplate,
-//   deleteCollection
-// };
+module.exports = {
+  deleteAsset,
+  deleteNFT,
+  deleteCanvas,
+  deleteTemplate,
+  deleteCollection,
+  updateCanvas,
+};
