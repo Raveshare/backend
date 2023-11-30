ab5f146b278da22d1a9fdf7a41f709402d4820b0
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2023-11-30 20:14:02 +0530

    docs: Removed console logs

--- src/routes/content/collectionRouter.js
+++ src/routes/content/collectionRouter.js
@@ -14,9 +14,6 @@ collectionRouter.get("/:collection/", async (req, res) => {
   let limit = req.query.limit || 50;
 
   let offset = limit * (page - 1);
-
-  // console.log("====================================");
-  // console.log(page, limit, offset);
   // this query can be cached again, as the collections are not changing frequently - so we can remove the cache middleware and cache till the asset are not updated
 
   let collectionsCache = await getCache(
@@ -72,8 +69,6 @@ collectionRouter.get("/:collection/", async (req, res) => {
   });
 
   let totalPage = Math.ceil(totalAssets / limit);
-  // console.log("====================================");
-  // console.log(contents[0]);
 
   res.send({
     assets: contents,
