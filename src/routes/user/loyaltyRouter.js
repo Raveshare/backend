f487c404666731611e6a035c744abdf9791c5a5c
aryan <agarwalaryan139@gmail.com>
2023-12-05 19:54:40 +0530

    feat: loyalty

--- src/routes/user/loyaltyRouter.js
+++ src/routes/user/loyaltyRouter.js
@@ -21,4 +21,55 @@ router.get("/invite-code", async (req, res) => {
   });
 });
 
+router.get("/tasks", async (req, res) => {
+
+  let tasks = await prisma.tasks.findMany({
+    where : {
+      locked: false,
+    },
+    select : {
+      id: true,
+      description: true,
+      amount: true,
+      campaign: true,
+    }
+  });
+
+  res.send({
+    message: tasks,
+  });
+});
+
+router.get("/reward-history", async (req, res) => {
+
+  let user_id = req.user.user_id;
+
+  let owner = await prisma.owners.findUnique({
+    where: {
+      id: user_id,
+    },
+    select: {
+      points: true,
+    },
+  });
+
+  let reward_history = await prisma.points_history.findMany({
+    where: {
+      ownerId: user_id,
+    },
+    select: {
+      taskId: true,
+      reason: true,
+      amount: true,
+      createdAt: true,
+    },
+  });
+
+  let response = [{"user_points":owner.points},...reward_history]
+
+  res.send({
+    message: response,
+  });
+});
+
 module.exports = router;
