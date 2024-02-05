001def8a751f1f3ca06d8773b9e8d8c74499afe8
Sugato Bagchi <sugato.bagchi.of@gmail.com>
2024-02-05 15:14:40 +0530

    fix: cronjob timings

--- src/utils/cronjobs/cronjob.js
+++ src/utils/cronjobs/cronjob.js
@@ -10,7 +10,8 @@ const {
   setCacheWithExpire,
 } = require("../../../src/functions/cache/handleCache.js");
 
-cron.schedule("*/10 * * * *", async () => {
+cron.schedule("0 * * * *", async () => {
+  console.log("Cron Job for Global Trending Mints is working");
   const cache = await getCache("trendingMints");
   await deleteCache("trendingMints");
   const trendingMintsData = await trendingMints();
@@ -21,7 +22,8 @@ cron.schedule("*/10 * * * *", async () => {
   );
 });
 
-cron.schedule("*/10 * * * *", async () => {
+cron.schedule("0 * * * *", async () => {
+  console.log("Cron Job for Global Trending Mints is working");
   const cache = await getCache("trendingMintsLenspost");
   await deleteCache("trendingMintsLenspost");
   const trendingMintsData = await trendingMintsLenspost();
